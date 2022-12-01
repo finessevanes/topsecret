61c7b02ed5ea4892b42ab2a2ea88538fea196e9b
Vanessa Mercado <finessevanes@Vanessas-MacBook-Pro.local>
2022-12-01 09:56:14 -0700

    3. Open QR Modal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import SignClient from "@walletconnect/sign-client";
+import { Web3Modal } from "@web3modal/standalone";
+import "./App.css";
+
+const web3Modal = new Web3Modal({
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  standaloneChains: ["eip155:5"],
+});
 
 function App() {
+  const [signClient, setSignClient] = useState();
+
+  async function createClient() {
+    try {
+      const signClient = await SignClient.init({
+        projectId: process.env.REACT_APP_PROJECT_ID,
+      });
+      setSignClient(signClient);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async function onConnect() {
+    if (!signClient) {
+      throw Error("Client is not set");
+    }
+    try {
+      const namespace = {
+        eip155: {
+          methods: ["eth_sign"],
+          chains: ["eip155:5"],
+          events: ["connect", "disconnect"],
+        },
+      };
+      const { uri } = await signClient.connect({
+        requiredNamespaces: namespace,
+      });
+
+      if (uri) {
+        web3Modal.openModal({ uri, standaloneChains: ["eip155:5"] });
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    if (!signClient) {
+      createClient();
+    }
+  }, [signClient]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sign v2 Standalone</h1>
+      <button onClick={onConnect} disabled={!signClient}>
+        Connect
+      </button>
     </div>
   );
 }
